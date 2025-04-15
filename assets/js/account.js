e3026a74418da047ccd1eef251feec8841b22110
Muhammad Halimov <muhammadi.halimov@icloud.com>
2025-04-16 02:14:36 +0400

    feat: major upgrades

--- assets/js/account.js
+++ assets/js/account.js
@@ -27,13 +27,29 @@ document.addEventListener("DOMContentLoaded", async () => {
             }
         });
 
+        let courses = await fetch('https://127.0.0.1:8000/api/progress', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
         if (!response.ok) {
-            console.error('Ошибка:', await response.json());
-            alert('Ошибка при получении данных. Возможно ваш аккаунт заблориван или не активирован.' + await response.json());
+            const errorData = await response.json();
+            console.error('Ошибка:', errorData.message);
+            alert('Ошибка при получении данных. Возможно ваш аккаунт заблориван или не активирован. ' + errorData.message);
             localStorage.removeItem('token');
             return window.location.href = 'auth.html';
         }
 
+        if (!courses.ok) {
+            const errorData = await courses.json();
+            console.error('Ошибка:', errorData.message);
+            alert('Ошибка при получении курсов. ' + errorData.message);
+        }
+
+        let course = await courses.json();
         let user = await response.json();
         let userFullName = (`${user.name} ${user.surname}`);
 
@@ -44,7 +60,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         document.getElementById('Patronym').value = user.patronym || '';
         document.getElementById('Phone').value = user.phone || '';
         document.getElementById('Email').value = user.email || '';
-        document.getElementById('DateOfBirth').value = user.dateOfBirth || '';
+        document.getElementById('DateOfBirth').value = new Date(user.dateOfBirth).toISOString().split('T')[0] || '';
         document.getElementById('AboutMe').value = user.message || '';
 
         // Приветственная страница в ЛК
@@ -56,14 +72,83 @@ document.addEventListener("DOMContentLoaded", async () => {
         // Курсы в ЛК
         const coursesList = document.getElementById('courses-list');
 
-        coursesList.innerHTML = user.courses?.length
-            ? user.courses.map(course => `<li>${course.title}</li>`).join('')
+        coursesList.innerHTML = course.progress.byCourse?.length
+            ? course.progress.byCourse.map(course => `<li>${course.courseTitle}</li>`).join('')
             : `<li>Нет курсов</li>`;
 
         // Прогресс в ЛК
+        const progressList = document.getElementById('courses-progress');
+
+        progressList.innerHTML = course.progress.byCourse?.length
+            ? course.progress.byCourse.map(course => `
+                <div class="m-b-15">
+                    <h5>${course.courseTitle}<span class="pull-right">${course.percentage}%</span></h5>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-primary wow animated progress-animated"
+                             style="width: ${course.percentage}%;">
+                        </div>
+                    </div>
+                </div>
+            `).join('')
+            : `<p>Пусто</p>`;
+
+        // Настройки аккаунта
+        let accountForm = document.getElementById('accountForm');
+
+        accountForm.addEventListener('submit', async function(e) {
+            e.preventDefault();
+
+            let password = document.getElementById('Password').value;
+            let rePassword = document.getElementById('RePassword').value;
+
+            if (password !== rePassword) {
+                alert('Пароли не совпадают.');
+                return;
+            }
+
+            let data = {
+                username: accountForm.username.value,
+                name: accountForm.name.value,
+                surname: accountForm.surname.value,
+                patronym: accountForm.patronym.value,
+                phone: accountForm.phone.value,
+                email: accountForm.email.value,
+                dateOfBirth: accountForm.dateOfBirth.value,
+                message: accountForm.message.value,
+                password: accountForm.password.value,
+                roles: ["ROLE_STUDENT"],
+            };
+
+            for (const [key, value] of Object.entries(data)) {
+                if (!value) {
+                    alert(`Поле "${key}" не заполнено`);
+                    return;
+                }
+            }
+
+            let updateProfileReques = await fetch(`https://127.0.0.1:8000/api/update-profile`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!updateProfileReques.ok) {
+                const errorData = await updateProfileReques.json();
+                console.error('Ошибка:', errorData.message);
+                alert('Ошибка при обновлении профиля. ' + errorData.message);
+            }
+
+            alert("Успешное обновление");
+            window.location.reload();
+        });
+
     } catch (error) {
         console.error('Ошибка:', error);
         alert('Ошибка сети. Попробуйте позже.');
+        window.location.href = 'index.html';
     }
 });
 
