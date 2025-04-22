482a11fed56a7fa98e12f86a081e416498e77178
unknown <IlyaShcherbakov2005@mail.ru>
2025-04-22 18:05:12 +0400

    fix: пофикшен конфиг файл и добавлен локальный конфиг

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ TOKEN = '7460973688:AAHFof34Yaw1PiS565QguH6zQtgEDYwYMos'
 
 server = "http://195.133.38.45/"
 
-img = "http://195.133.38.45/img/"
+img = "http://195.133.38.45/images/"
 
-profile_photos = "http://195.133.38.45/img/profile_photos/"
+profile_photos = "http://195.133.38.45/images/profile_photos/"
