9cc407c71a5543612c984aaf6355efaeeb2f8633
Muhammad Halimov <muhammadi.halimov@icloud.com>
2025-04-25 19:05:28 +0400

    style: connect tg user with base

--- assets/js/account.js
+++ assets/js/account.js
@@ -463,3 +463,42 @@ async function onTelegramAuth(user) {
         alert(`Ошибка при привязке профиля ТГ: ${error.message}`);
     }
 }
+
+async function checkTelegramUser() {
+    try {
+        const tgButtonIframe = document.querySelector('script[data-telegram-login="autoschoolmybuddybot"]');
+        const accountForm = document.getElementById('accountForm');
+        const submitButton = accountForm.querySelector('button[type="submit"]');
+        const userId = localStorage.getItem('userId');
+
+        let getUserProfileFetch = await fetch(`https://${urlAddress}/api/users/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        let user = await getUserProfileFetch.json();
+
+        if (user.telegramId) {
+            // Скрываем iframe с кнопкой Telegram (если он есть)
+            const tgIframe = document.getElementById('telegram-login-autoschoolmybuddybot');
+            if (tgIframe) tgIframe.style.display = 'none';
+
+            // Создаем кнопку "Профиль привязан к ТГ"
+            const boundButton = document.createElement('button');
+            boundButton.className = 'btn btn-primary waves-effect waves-light w-md';
+            boundButton.type = 'button';
+            boundButton.disabled = true;
+            boundButton.textContent = 'Профиль привязан к ТГ';
+            boundButton.style.cssText = 'width: 225px; height: 40px; margin-bottom: 33px; margin-left: 10px;';
+
+            // Вставляем новую кнопку рядом с основной
+            submitButton.insertAdjacentElement('afterend', boundButton);
+        }
+    }
+    catch (error) {
+        console.error(`Ошибка профиля ТГ: ${error.message}`);
+    }
+}
