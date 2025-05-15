205c4003639e5c79952bb9904d0edc607e8e0a6d
muhammad-halimov <muhammadi.halimov@icloud.com>
2025-05-15 13:22:57 +0400

    fix: import optimization

--- src/Controller/Admin/CourseQuizCrudController.php
+++ src/Controller/Admin/CourseQuizCrudController.php
@@ -9,7 +9,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CourseQuizCrudController extends AbstractCrudController
